b8445583d8aa849d94404f056a5705f74ce24712
Jason <jasonzhe@usc.edu>
2017-03-07 19:42:32 -0800

    update home data

--- src/shared/components/home.js
+++ src/shared/components/home.js
@@ -18,61 +18,52 @@ class Home extends Component {
         </div>
         <div className="margin-bottom-4">
           <p className="h2 margin-bottom-4">
-            Hi there! I'm <b>Christian Le</b>,
+            Hey, I'm <b>Jason Zheng</b>,
           </p>
           <p className="h3 light lineheight-m">
             a <b>
-              <a href="https://github.com/cle1994" target="_blank">
+              <a href="https://github.com/xlaefz" target="_blank">
                 software engineer
               </a>
             </b>
             &nbsp;born and raised in <b>
-              <a href="https://goo.gl/Jpbpks" target="_blank">
-                Southern California
-              </a>
-            </b> currently residing in the <b>
-              <a href="https://goo.gl/knXZm4" target="_blank">
-                Bay Area
-              </a>
-            </b> as a recent graduate from <b>
-              <a href="http://www.berkeley.edu/" target="_blank">
-                UC Berkeley
-              </a>
-            </b>. I co-founded and am currently working at <b>
-              <a href="https://www.outcomes.com" target="_blank">
-                Outcomes.com
+              Southern California
+            </b> currently residing in Los Angeles.
+            <b>
+              <a href="http://www.usc.edu/" target="_blank">
+                USC
               </a>
             </b>.
           </p>
         </div>
         <div className="margin-bottom-4">
           <p className="h4 light margin-bottom-2">
-            Welcome to my website, a place that holds a small collection of my
+            Welcome to my website,where I
             <b>
               <Link to={'/photos'}>
-                &nbsp;photos
+                &nbsp;blog
               </Link>
-            </b>, a place where I can host my
+            </b>, where I showcase my
             <b>
               <Link to={'/projects'}>
-                &nbsp;rants and projects
+                &nbsp;projects
               </Link>
             </b> about anything, a place that I use to practice my web
             development, and at the end of the day, a place about me.
             So a little about me...
           </p>
           <p className="h4 light margin-bottom-2">
-            I spend my free time flying quadcopters, drinking boba/tea,
-            catching up on TV shows, attemping to go on photo adventures, and
-            contemplating when I'll next pick up my tennis racket.
+            I spend alot of my time weigt lifting and looking at cool app designs.
+            I personally don't do alot of design work; however, I really appreciate
+             it and think it's pretty cool.
           </p>
         </div>
         <div className="links">
           <div className="link">
-            <a href="https://github.com/cle1994" target="_blank">GitHub</a>
+            <a href="https://github.com/xlaefz" target="_blank">GitHub</a>
           </div>
           <div className="link">
-            <a href="https://www.linkedin.com/in/jasonsuzheng" target="_blank">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/jason-zheng-88854592/" target="_blank">LinkedIn</a>
           </div>
           <div className="link">
             <Link to={'/message'}>Message</Link>
